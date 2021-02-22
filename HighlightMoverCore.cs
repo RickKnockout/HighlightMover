9d4edbe03cdddeedd4a0204748427effda4adb5e
cruentus <rickkayo@gmail.com>
2021-02-21 19:52:34 -0800

    Update HighlightMoverCore.cs

--- HighlightMoverCore.cs
+++ HighlightMoverCore.cs
@@ -68,9 +68,9 @@ namespace HighlightMover
                 if (item.isHighlighted)
                 {
                     MoveMouseToElement(item.GetClientRect().Center);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
                     Input.KeyDown(System.Windows.Forms.Keys.LControlKey);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
             
                     uiTabsOpened = GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible &&
                               GameController.Game.IngameState.IngameUi.StashElement.IsVisibleLocal;
@@ -83,17 +83,17 @@ namespace HighlightMover
                     }
 
                     Input.Click(System.Windows.Forms.MouseButtons.Left);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
                     Input.Click(System.Windows.Forms.MouseButtons.Left);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
                     Input.Click(System.Windows.Forms.MouseButtons.Left);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
 
                     Input.KeyUp(System.Windows.Forms.Keys.LControlKey);
-                    yield return new WaitTime(5);
+                    yield return new WaitTime(2);
                 }
 
-                yield return new WaitTime(rand.Next(10, 25));
+                yield return new WaitTime(rand.Next(2, 5));
             }
             running = false;
         }
